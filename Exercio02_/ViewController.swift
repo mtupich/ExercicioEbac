b4b488fd1c7b12ffa9e8aa70d4dc88ec3724d2bd
Maria Eduarda Tupich <maria.tupich@br.experian.com>
2022-01-24 08:34:15 -0300

    commit inicial

---
+++ Exercio02_/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Exercio02_
+//
+//  Created by C94279A on 09/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var uiimageView: UIImageView!
+    @IBOutlet weak var uiLabelo: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
